45794673bc8b6659d182898f58a447eb65124f00
Hakel <hakim_013@hotmail.fr>
2024-08-05 10:50:16 +0200

    Derniere ligne droite

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+
+// ALERTE NAVIGATEUR POUR LE FORM
+
 document.getElementById("contactform").addEventListener('submit', function(event) {
     event.preventDefault(); // Empêche l'envoi du formulaire
-    
+
     const nom = document.getElementById("nom").value;
     const prenom = document.getElementById("prenom").value;
     const email = document.getElementById("email").value;
@@ -16,3 +19,21 @@ document.getElementById("contactform").addEventListener('submit', function(event
 });
 
 
+// DARK MODE 
+
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton= document.getElementById("toggle-dark-mode");
+    const body= document.body;
+
+    // Vérifie si le mode sombre est déjà activé (si la classe "dark-mode" est appliquée)
+    const isDarkMode = body.classList.contains('dark-mode');
+
+    // Met à jour le texte du bouton en fonction de l'état actuel
+    toggleButton.textContent = isDarkMode ? 'Activer le mode clair' : 'Activer le mode sombre';
+
+    toggleButton.addEventListener('click', () => {
+    // Bascule la classe "dark-mode" sur le body
+    body.classList.toggle('dark-mode');
+});
+
+});
